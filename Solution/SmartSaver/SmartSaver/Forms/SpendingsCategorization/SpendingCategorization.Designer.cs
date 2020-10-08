507ae2049ec29781b728748457215dd0e9746df2
aurimasjurgelis <dev.aurimas@gmail.com>
2020-10-08 15:09:07 +0300

    test2

--- Solution/SmartSaver/SmartSaver/Forms/SpendingsCategorization/SpendingCategorization.Designer.cs
+++ Solution/SmartSaver/SmartSaver/Forms/SpendingsCategorization/SpendingCategorization.Designer.cs
@@ -28,10 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpendingCategorization));
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.backButton = new System.Windows.Forms.Button();
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,8 +42,9 @@
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(791, 104);
+            this.panel1.Size = new System.Drawing.Size(1006, 139);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -58,10 +61,14 @@
             // backButton
             // 
             this.backButton.BackColor = System.Drawing.Color.White;
+            this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.backButton.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.backButton.Location = new System.Drawing.Point(12, 110);
+            this.backButton.Image = ((System.Drawing.Image)(resources.GetObject("backButton.Image")));
+            this.backButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.backButton.Location = new System.Drawing.Point(14, 147);
+            this.backButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(115, 47);
+            this.backButton.Size = new System.Drawing.Size(131, 63);
             this.backButton.TabIndex = 1;
             this.backButton.Text = "Back";
             this.backButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -70,25 +77,38 @@
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.label1);
             this.mainPanel.Controls.Add(this.backButton);
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(791, 578);
+            this.mainPanel.Size = new System.Drawing.Size(1006, 653);
             this.mainPanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(367, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Food";
             // 
             // SpendingCategorization
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(227)))), ((int)(((byte)(250)))));
-            this.ClientSize = new System.Drawing.Size(791, 578);
+            this.ClientSize = new System.Drawing.Size(1006, 653);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.mainPanel);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SpendingCategorization";
             this.Text = "SpendingCategorization";
             this.Load += new System.EventHandler(this.SpendingCategorization_Load);
             this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -99,5 +119,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button backButton;
         private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label label1;
     }
 }
