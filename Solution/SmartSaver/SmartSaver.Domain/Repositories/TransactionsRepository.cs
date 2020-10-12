6b5cc94efe5f8e33f6cdfa9af9a426b5a002a712
Tomas Kondrotas <tomasskon@gmail.com>
2020-10-13 00:11:24 +0300

    Creating Domain layer of the project/adding generic repos and creating connection to db

---
+++ Solution/SmartSaver/SmartSaver.Domain/Repositories/TransactionsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartSaver.Domain.Models;
+
+namespace SmartSaver.Domain.Repositories
+{
+    public class TransactionsRepository : GenericRepository<Transaction>
+    {
+        public async Task<IReadOnlyList<Transaction>> GetUserTransactions(Guid userId)
+        {
+            return await Set.Where(i => i.UserId == userId).ToListAsync();
+        }
+    }
+}
