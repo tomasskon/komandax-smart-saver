1617ff8ef52883bc3530c61d358486f04aacf9af
aurimasjurgelis <dev.aurimas@gmail.com>
2020-10-16 11:17:56 +0300

    final

--- Solution/SmartSaver/SmartSaver/Forms/UserControls/Savings.cs
+++ Solution/SmartSaver/SmartSaver/Forms/UserControls/Savings.cs
@@ -5,19 +5,24 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using SmartSaver.Forms.Savings;
 
 namespace SmartSaver.Forms.UserControls
 {
     public partial class Savings : UserControl
     {
+        
+        SavingsGoalForm savingsGoalForm;
         public Savings()
         {
             InitializeComponent();
+            savingsGoalForm = new SavingsGoalForm();
+            
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-
+            
         }
 
         private void panel4_Paint(object sender, PaintEventArgs e)
