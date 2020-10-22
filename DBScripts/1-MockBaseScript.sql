b7759096ee10e9837530851e63540dad28e36912
Ignas Gilys <gilys.ignas@gmail.com>
2020-10-22 19:04:46 +0300

    Seperating scripts.

--- DBScripts/1-MockBaseScript.sql
+++ DBScripts/1-MockBaseScript.sql
@@ -39,22 +39,3 @@ GO
 
 INSERT INTO [Transactions] ([Amount], [Description], [UserId]) VALUES (7, 'Buying Pizza', '6e33fa08-bc0f-438c-a21b-bcf4fc227661')
 GO
-
-CREATE TABLE [dbo].[Balances](
-    [Id] [uniqueidentifier] NOT NULL,
-    [Cash] [float] NULL,
-    [Card] [float] NULL,
-    [UserId] [uniqueidentifier] NOT NULL
- CONSTRAINT [PK_Balances] PRIMARY KEY CLUSTERED 
-(
-    [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Balances] ADD  CONSTRAINT [DF_Balances_Id]  DEFAULT (newsequentialid()) FOR [Id]
-GO
-
-ALTER TABLE [dbo].[Balances]  WITH CHECK ADD  CONSTRAINT [FK_Balances_Users] FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([Id])
-GO
