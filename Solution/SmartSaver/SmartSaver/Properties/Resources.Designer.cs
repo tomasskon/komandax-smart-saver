c8584b93c260179be0ac6b09369e1f15e190e4d1
aurimasjurgelis <dev.aurimas@gmail.com>
2020-10-23 10:18:52 +0300

    init a new branch

--- Solution/SmartSaver/SmartSaver/Properties/Resources.Designer.cs
+++ Solution/SmartSaver/SmartSaver/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace SmartSaver.Presentation.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-
     }
 }
