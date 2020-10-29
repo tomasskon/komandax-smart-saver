b98d2fcdc883d159aa6fff748d3038328109cfd5
Tomas Kondrotas <tomasskon@gmail.com>
2020-10-29 23:17:20 +0200

    adding repos/models/dbsets

---
+++ Solution/SmartSaver/SmartSaver.Domain/Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartSaver.Domain.Models
+{
+    public class UserLogin
+    {
+        public Guid UserId { get; set; }
+
+        public byte[] LoginData { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Solution/SmartSaver/SmartSaver.Domain/Repositories/UserLoginsRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartSaver.Domain.Models;
+
+namespace SmartSaver.Domain.Repositories
+{
+    public class UserLoginsRepository
+    {
+        private protected readonly SmartSaverDataContext Context = new SmartSaverDataContext(new DbContextOptionsBuilder<SmartSaverDataContext>()
+            .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SmartSaver;Integrated Security=True").Options);
+        private protected DbSet<UserLogin> Set => Context.Set<UserLogin>();
+
+        public async Task<byte[]> GetUserLogin(Guid userId)
+        {
+            return (await Set.SingleOrDefaultAsync(s => s.UserId == userId))?.LoginData;
+        }
+
+        public async Task CreateUserLogin(Guid userId, byte[] loginData)
+        {
+            var userLogin = new UserLogin
+            {
+                UserId = userId,
+                LoginData = loginData
+            };
+
+            Set.Add(userLogin);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
