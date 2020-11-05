a22f8d50818dd818d67b63214596d6e3ff779796
Ignas Gilys <gilys.ignas@gmail.com>
2020-11-05 14:26:33 +0200

    Correcting mistakes, undo-ing accidental file deletion.

--- Solution/SmartSaver/SmartSaver/Forms/UserControls/Main.cs
+++ Solution/SmartSaver/SmartSaver/Forms/UserControls/Main.cs
@@ -10,6 +10,9 @@ using SmartSaver.Logic.HelperClasses.Images;
 using SmartSaver.Domain.Repositories;
 using SmartSaver.Logic.HelperClasses.Balance;
 using SmartSaver.Domain.Models;
+using SmartSaver.Logic.HelperClasses.Transactions;
+using SmartSaver.Presentation.Helpers;
+using SmartSaver.Logic.HelperClasses.Categories;
 
 namespace SmartSaver.Forms.UserControls
 {
@@ -20,16 +23,18 @@ namespace SmartSaver.Forms.UserControls
         {
             InitializeComponent();
             UpdateInfo();
+            ReloadLastTransactions();
+            ReloadSpentPerCategoryList();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
             PictureBox p = sender as PictureBox;
-            if(p != null)
+            if (p != null)
             {
-                open.Filter = "(*.jpg;*.jpeg;*.bmp;*.png)| *.jpg; *.jpeg; *.bmp; *.png" ;
-                if(open.ShowDialog() == DialogResult.OK)
+                open.Filter = "(*.jpg;*.jpeg;*.bmp;*.png)| *.jpg; *.jpeg; *.bmp; *.png";
+                if (open.ShowDialog() == DialogResult.OK)
                 {
                     p.Image = Image.FromFile(open.FileName);
                 }
@@ -38,13 +43,13 @@ namespace SmartSaver.Forms.UserControls
 
         private byte[] ImageToByteArray(System.Drawing.Image imageIn)
         {
-            if(pictureBox1.Image != null)
-            using (MemoryStream ms = new MemoryStream())
-            {
+            if (pictureBox1.Image != null)
+                using (MemoryStream ms = new MemoryStream())
+                {
                     Bitmap bmp = new Bitmap(imageIn);
                     bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
                     return ms.ToArray();
-            }
+                }
             return null;
         }
 
@@ -58,23 +63,25 @@ namespace SmartSaver.Forms.UserControls
             await _userRepository.Update(_user.Id, _user);
             string text = "Image successfully saved!";
             MessageBox.Show(text);
-        
+
         }
 
         public Image ByteArrayToImage(byte[] ba)
         {
-            if(ba != null)
-            using (MemoryStream ms = new MemoryStream(ba))
-            {
-                Image returnImage = Image.FromStream(ms);
-                return returnImage;
-            }
+            if (ba != null)
+                using (MemoryStream ms = new MemoryStream(ba))
+                {
+                    Image returnImage = Image.FromStream(ms);
+                    return returnImage;
+                }
             return null;
         }
 
         private void RefreshClick(object sender, EventArgs e)
         {
             UpdateInfo();
+            ReloadLastTransactions();
+            ReloadSpentPerCategoryList();
         }
 
         public async void UpdateInfo()
@@ -82,13 +89,83 @@ namespace SmartSaver.Forms.UserControls
             var _userRepository = new UserRepository();
             var helper = new BalanceHelper(_userRepository);
             _user = await helper.GetUserBalance(userId: Domain.Constants.Constants.TestUserId);
-            
-#pragma warning disable CS0472 
+
+#pragma warning disable CS0472
             if (_user.Cash + _user.Card != null)
                 textBox1.Text = (_user.Cash + _user.Card).FormatMoney();
-            if(_user.UserImage != null)
+            if (_user.UserImage != null)
                 pictureBox1.Image = ByteArrayToImage(_user.UserImage);
             await _userRepository.Update(_user.Id, _user);
+        }
+
+        private ListViewItem[] GetTransactionsListViewItems(IReadOnlyList<Transaction> transactions)
+        {
+            var listViewItems = new List<ListViewItem>();
+
+            foreach (var transaction in transactions)
+            {
+                var item = new ListViewItem(transaction.AmountDouble.FormatMoney());
+                item.SubItems.Add(transaction.Category.Name);
+                item.SubItems.Add(transaction.BalanceType);
+                item.SubItems.Add(transaction.CreatedAt.ToString("yyyy-MM-dd HH:mm"));
+
+                listViewItems.Add(item);
+            }
+
+            return listViewItems.ToArray();
+        }
+
+        private async void ReloadLastTransactions()
+        {
+            var transactionHelper = new TransactionsHelper(new TransactionsRepository());
+            try
+            {
+                var transactions = await transactionHelper
+                    .GetLastUserTransactions(Domain.Constants.Constants.TestUserId, 5);
+
+                var listViewItems = GetTransactionsListViewItems(transactions);
+
+                lastTransactions.Items.Clear();
+                lastTransactions.Items.AddRange(listViewItems);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Error.ShowDialog(ex.Message);
+            }
+        }
+
+        private ListViewItem[] GetSpentPerCategoryListViewItems(IReadOnlyList<GroupedTransaction> transactions)
+        {
+            var listViewItems = new List<ListViewItem>();
+
+            foreach (var transaction in transactions)
+            {
+                var item = new ListViewItem(transaction.Key);
+                item.SubItems.Add(transaction.SumDouble.FormatMoney());
+
+                listViewItems.Add(item);
+            }
+
+            return listViewItems.ToArray();
+        }
+
+        private async void ReloadSpentPerCategoryList()
+        {
+            var categoriesHelper = new TransactionsHelper(new TransactionsRepository());
+            try
+            {
+                var transactions = await categoriesHelper
+                    .GetAmountSpentPerCategory(Domain.Constants.Constants.TestUserId);
+
+                var listViewItems = GetSpentPerCategoryListViewItems(transactions);
+
+                spentPerCategoryList.Items.Clear();
+                spentPerCategoryList.Items.AddRange(listViewItems);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Error.ShowDialog(ex.Message);
+            }
         }
 
     }
