3087b1928fdb39d5e0f08dc64bfcaf1fd526758b
Ignas Gilys <gilys.ignas@gmail.com>
2020-11-20 10:41:57 +0200

    port issues test

---
+++ SmartSaver/SmartSaver.Presentation/SmartSaver.Presentation/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSaver.Presentation.Models
+{
+    public class User
+    {
+        public string Gmail { get; set; }
+
+        public double Cash { get; set; }
+
+        public double Card { get; set; }
+
+        public string FullName { get; set; }
+
+        public byte[] UserImage { get; set; }
+    }
+}
