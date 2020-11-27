376846c8d0c6e7e60515b883eeabb8ed147d414f
Tomas Kondrotas <tomasskon@gmail.com>
2020-11-27 03:00:50 +0200

    Adding transactions sorting/reusable transactions buttons/loading spinner

---
+++ SmartSaver/SmartSaver.Presentation/SmartSaver.Presentation/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartSaver.Presentation.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
---
+++ SmartSaver/SmartSaver.Presentation/SmartSaver.Presentation/Models/TransactionsSortingModel.cs
@@ -0,0 +1,11 @@
+﻿using SmartSaver.Presentation.Pages.Transactions;
+
+namespace SmartSaver.Presentation.Models
+{
+    public class TransactionsSortingModel
+    {
+        public TransactionsSortingEnum SortingColumn { get; set; } = TransactionsSortingEnum.CreatedAt;
+
+        public bool IsAscending { get; set; } = true;
+    }
+}
