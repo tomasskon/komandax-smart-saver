c95a6cd2bbb5045bf4d5b5a97f0db8fa6b7b7bee
Tomas Kondrotas <tomasskon@gmail.com>
2020-12-04 03:09:05 +0200

    Adding breadcrumb system

---
+++ SmartSaver/SmartSaver.Presentation/SmartSaver.Presentation/Shared/Breadcrumbs/IBreadcrumbService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SmartSaver.Presentation.Models;
+
+namespace SmartSaver.Presentation.Shared.Breadcrumbs
+{
+    public interface IBreadcrumbService
+    {
+        event Action BreadcrumbsChanged;
+
+        void InvokeBreadcrumbsChanged();
+
+        public void AddBreadcrumb(string breadcrumbDisplayName, string url);
+
+        public void ClearList();
+
+        IReadOnlyList<Breadcrumb> GetBreadcrumbsList();
+    }
+}
