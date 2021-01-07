1214f340817f58937d6404e230513ab070d35d5a
Ignas Gilys <gilys.ignas@gmail.com>
2021-01-07 23:53:37 +0200

    adding image upload to directory (failed to add to database + ignore the many images) and updated profile page, started console app to fulfill requirements

---
+++ SmartSaver/SmartSaver.Server/SmartSaver.Server/Controllers/ImagesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartSaver.Domain.Models;
+using SmartSaver.Domain.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Web;
+
+namespace SmartSaver.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IHostEnvironment _environment;
+
+        public ImagesController(IHostEnvironment environment)
+        {
+            this._environment = environment;
+        }
+        [HttpGet("{link}")]
+        public IActionResult GetImage(string link)
+        {
+            return PhysicalFile($@"{_environment.ContentRootPath}\wwwroot\images\{link}", "image/png");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] IFormFile image)
+        {
+            if (image == null || image.Length == 0)
+                return BadRequest("Please upload a file.");
+
+            string fileName = image.FileName;
+            string extension = Path.GetExtension(fileName);
+            string[] allowedExtensions = { ".JPG", ".jpg", ".png", ".bmp" };
+
+            if (!allowedExtensions.Contains(extension))
+                return BadRequest("File is not an image");
+            string newFileName = $"{Guid.NewGuid()}{extension}";
+            string filePath = Path.Combine(_environment.ContentRootPath,"wwwroot","Images", newFileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                await image.CopyToAsync(fileStream);
+            }
+            return Ok($"api/Images/{newFileName}");
+        }
+    
+
+    }
+}
---
+++ SmartSaver/SmartSaver.Server/SmartSaver.Server/Data/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace SmartSaver.Server.Data
+{
+    public partial class Data
+    {
+        protected void Fill()
+        {
+            string sqlConnectString = "Server=(localdb)\\mssqllocaldb;Database=SmartSaver;Integrated Security=True";
+
+            string sqlSelect = "SELECT FullName FROM dbo.Users";
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlSelect, sqlConnectString);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            foreach (DataRow row in dt.Rows)
+                Console.WriteLine("{0} {1}", row[0], row["Fullname"]);
+
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Contact");
+
+            Console.WriteLine(ds.Tables.Count);
+            Console.WriteLine(ds.Tables[0].TableName);
+            foreach (DataRow row in ds.Tables["Users"].Rows)
+                Console.WriteLine("{0} {1}", row[0], row[1]);
+        }
+    }
+}
