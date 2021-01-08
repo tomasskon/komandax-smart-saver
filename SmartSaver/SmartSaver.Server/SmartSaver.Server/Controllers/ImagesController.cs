19a6218aebf4499d6de7513796b3866b00689df3
Marius Kasparavičius <marius.kasparavicius@mif.stud.vu.lt>
2021-01-08 02:03:53 +0200

    Get user image

--- SmartSaver/SmartSaver.Server/SmartSaver.Server/Controllers/ImagesController.cs
+++ SmartSaver/SmartSaver.Server/SmartSaver.Server/Controllers/ImagesController.cs
@@ -17,30 +17,45 @@ namespace SmartSaver.Server.Controllers
     public class ImagesController : ControllerBase
     {
         private readonly IHostEnvironment _environment;
+        private readonly IUsersRepository _usersRepository;
 
-        public ImagesController(IHostEnvironment environment)
+        public ImagesController(IHostEnvironment environment, IUsersRepository usersRepository)
         {
             this._environment = environment;
+            this._usersRepository = usersRepository;
         }
         [HttpGet("{link}")]
         public IActionResult GetImage(string link)
         {
-            return PhysicalFile($@"{_environment.ContentRootPath}\wwwroot\images\{link}", "image/png");
+            var filename = $@"{_environment.ContentRootPath}\wwwroot\images\{link}";
+
+            if (System.IO.File.Exists(filename))
+            {
+                return PhysicalFile(filename, "image/jpg");
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromForm] IFormFile image)
+        public async Task<IActionResult> Post([FromForm] IFormFile image, [FromForm] Guid userId)
         {
             if (image == null || image.Length == 0)
                 return BadRequest("Please upload a file.");
+            var user = _usersRepository.GetById(userId);
+            if (user == null)
+                return BadRequest("User does not exist");
 
             string fileName = image.FileName;
             string extension = Path.GetExtension(fileName);
-            string[] allowedExtensions = { ".JPG", ".jpg", ".png", ".bmp" };
+            string[] allowedExtensions = { ".JPG", ".jpg" };
 
             if (!allowedExtensions.Contains(extension))
                 return BadRequest("File is not an image");
-            string newFileName = $"{Guid.NewGuid()}{extension}";
+
+            string newFileName = $"{userId}.jpg";
             string filePath = Path.Combine(_environment.ContentRootPath,"wwwroot","Images", newFileName);
 
             using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
